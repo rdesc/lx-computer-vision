a320b91aaf5224361ae3051c095452fe5f2ddcd9
rdesc <rodrigue.de.schaetzen@gmail.com>
2024-11-11 17:59:18 -0500

    done hw3

--- packages/visual_lane_servoing/include/visual_servoing_solution.py
+++ packages/visual_lane_servoing/include/visual_servoing_solution.py
@@ -3,6 +3,10 @@ from typing import Tuple
 import numpy as np
 import cv2
 from scipy.ndimage import label
+import rospy
+
+rospy.set_param('left_gain', 1)
+rospy.set_param('right_gain', 1)
 
 
 def get_steer_matrix_left_lane_markings(shape: Tuple[int, int]) -> np.ndarray:
@@ -15,33 +19,37 @@ def get_steer_matrix_left_lane_markings(shape: Tuple[int, int]) -> np.ndarray:
                             using the masked left lane markings (numpy.ndarray)
     """
     H, W = shape
+    steer_matrix_left = np.ones(shape)
 
-    # Create the reference lane marking masks with lines
-    left_lane_ref = np.zeros((H, W), dtype=np.uint8)
+    # # Create the reference lane marking masks with lines
+    # left_lane_ref = np.zeros((H, W), dtype=np.uint8)
 
-    # Specify the reference angles for the lane
-    left_lane_ref_degree = 45 * np.pi / 180
-    thickness_line = 20
-    scaling_factor = 1
+    # # Specify the reference angles for the lane
+    # left_lane_ref_degree = 45 * np.pi / 180
+    # thickness_line = 40
+    # scaling_factor = 1 / 10000
 
-    # Draw the reference lines
-    cv2.line(left_lane_ref, (0, H), (int(H / np.tan(left_lane_ref_degree)), 0), 255, thickness_line)
+    # # Draw the reference lines
+    # cv2.line(left_lane_ref, (0, H), (int(H / np.tan(left_lane_ref_degree)), 0), 255, thickness_line)
 
-    # Find the regions on each side of the reference lines
-    line_mask = left_lane_ref == 255
-    labeled_array_left, num_features = label(~line_mask)  # Invert the line mask for regions
+    # # Find the regions on each side of the reference lines
+    # line_mask = left_lane_ref == 255
+    # labeled_array_left, num_features = label(~line_mask)  # Invert the line mask for regions
 
-    # do distance transform
-    dist_left_line = cv2.distanceTransform(~left_lane_ref, cv2.DIST_L2, 5)
-    dist_left_line[labeled_array_left == 1] *= -1
+    # # do distance transform
+    # dist_left_line = cv2.distanceTransform(~left_lane_ref, cv2.DIST_L2, 5)
+    # dist_left_line[labeled_array_left == 2] *= -1
 
-    # ignore half of the image
-    dist_left_line[:, W // 2:] = 0
+    # # ignore half of the image
+    steer_matrix_left[:, W // 2:] = 0
 
-    # normalize by the max value
-    steer_matrix_left = dist_left_line / np.max(np.abs(dist_left_line)) * scaling_factor
+    # # normalize by the max value
+    # steer_matrix_left = dist_left_line / np.max(np.abs(dist_left_line)) * scaling_factor
+    # max_value = np.percentile(np.abs(steer_matrix_left), q=95)
+    # steer_matrix_left[steer_matrix_left > max_value] = max_value
+    # steer_matrix_left[steer_matrix_left < -max_value] = -max_value
 
-    return steer_matrix_left
+    return steer_matrix_left * -1 * rospy.get_param('left_gain') # -1 is right turn and +1 is left turn
 
 
 def get_steer_matrix_right_lane_markings(shape: Tuple[int, int]) -> np.ndarray:
@@ -56,33 +64,38 @@ def get_steer_matrix_right_lane_markings(shape: Tuple[int, int]) -> np.ndarray:
 
     H, W = shape
 
-    # Create the reference lane marking masks with lines
-    right_lane_ref = np.zeros((H, W), dtype=np.uint8)
+    steer_matrix_right = np.ones(shape)
 
-    # Specify the reference angles for the lane
-    right_lane_ref_degree = 45 * np.pi / 180
-    thickness_line = 20
-    scaling_factor = 1
+    # # Create the reference lane marking masks with lines
+    # right_lane_ref = np.zeros((H, W), dtype=np.uint8)
 
-    # Draw the reference lines
-    cv2.line(right_lane_ref, (W, H), (int(W - H / np.tan(right_lane_ref_degree)), 0), 255, thickness_line)
+    # # Specify the reference angles for the lane
+    # right_lane_ref_degree = 45 * np.pi / 180
+    # thickness_line = 40
+    # scaling_factor = 1 / 10000
 
-    # Find the regions on each side of the reference lines
-    line_mask = right_lane_ref == 255
-    labeled_array_right, num_features = label(~line_mask)  # Invert the line mask for regions
+    # # Draw the reference lines
+    # cv2.line(right_lane_ref, (W, H), (int(W - H / np.tan(right_lane_ref_degree)), 0), 255, thickness_line)
 
-    # do distance transform
-    dist_right_line = cv2.distanceTransform(~right_lane_ref, cv2.DIST_L2, 5)
+    # # Find the regions on each side of the reference lines
+    # line_mask = right_lane_ref == 255
+    # labeled_array_right, num_features = label(~line_mask)  # Invert the line mask for regions
 
-    dist_right_line[labeled_array_right == 1] *= -1
+    # # do distance transform
+    # dist_right_line = cv2.distanceTransform(~right_lane_ref, cv2.DIST_L2, 5)
 
-    # ignore half of the image
-    dist_right_line[:, :W // 2] = 0
+    # dist_right_line[labeled_array_right == 2] *= -1
 
-    # normalize by the max value
-    steer_matrix_right = dist_right_line / np.max(np.abs(dist_right_line)) * scaling_factor
+    # # ignore half of the image
+    steer_matrix_right[:, :W // 2] = 0
 
-    return steer_matrix_right
+    # # normalize by the mean value
+    # steer_matrix_right = dist_right_line / np.max(np.abs(dist_right_line)) * scaling_factor
+    # max_value = np.percentile(np.abs(steer_matrix_right), q=95)
+    # steer_matrix_right[steer_matrix_right > max_value] = max_value
+    # steer_matrix_right[steer_matrix_right < -max_value] = -max_value
+
+    return steer_matrix_right * rospy.get_param('right_gain') * 0.7
 
 def detect_lane_markings(image: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
     """
@@ -98,11 +111,14 @@ def detect_lane_markings(image: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
     threshold_left = 5  # gradient magnitude threshold
     threshold_right = 20
     sigma = 8       # gaussian blurring threshold
-    white_lower_hsv = np.array([0, 0, 0])
-    white_upper_hsv = np.array([179, 70, 100])
+    # white_lower_hsv = np.array([35, 0, 0])
+    # white_upper_hsv = np.array([179, 70, 100])
     
-    yellow_lower_hsv = np.array([15, 100, 100])
-    yellow_upper_hsv = np.array([35, 255, 255])
+    white_lower_hsv = np.array([0, 0, 200]) # 0 0 150
+    white_upper_hsv = np.array([179, 50, 255])  # 179 80 255
+
+    yellow_lower_hsv = np.array([10, 80, 40])
+    yellow_upper_hsv = np.array([70, 255, 255])
     # yellow_lower_hsv = np.array([0, 0, 0])
     # yellow_upper_hsv = np.array([179, 255, 255])
 
